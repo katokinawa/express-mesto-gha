0cefec055f40677e1264e8a203612f319b64c8ed
Daniil Belogur <katokinawa@icloud.com>
2023-01-11 17:49:02 +0300

    feat: user.js models routes and controllers

---
+++ controllers/user.js
@@ -0,0 +1,24 @@
+const User = require("../models/user");
+
+module.exports.getUsers = (req, res) => {
+  // Получить массив пользователей, то есть всех
+  User.find({})
+    .then((users) => res.send({ data: users }))
+    .catch((err) => res.status(500).send({ message: err.message }));
+};
+
+module.exports.findUserById = (req, res) => {
+  // Найти по ID
+  User.findById(req.params.id)
+    .then((user) => res.send({ data: user }))
+    .catch((err) => res.status(500).send({ message: err.message }));
+};
+
+module.exports.createUser = (req, res) => {
+  // Создать пользователя
+  const { name, about, avatar } = req.body;
+
+  User.create({ name, about, avatar })
+    .then((user) => res.send({ data: user }))
+    .catch((err) => res.status(500).send({ message: err.message }));
+};
