8fcc7cb39facf7e0d6959a71c6b289e9e7f1e62e
Daniil Belogur <katokinawa@icloud.com>
2023-01-25 23:31:26 +0300

    feat: model user.js and controllers hash for pass

--- controllers/users.js
+++ controllers/users.js
@@ -1,4 +1,5 @@
 const User = require('../models/user');
+const bcrypt = require('bcryptjs');
 
 const badRequest = 400;
 const notFound = 404;
@@ -38,9 +39,10 @@ module.exports.findUserById = (req, res) => {
 
 module.exports.createUser = (req, res) => {
   // Создать пользователя
-  const { name, about, avatar } = req.body;
-
-  User.create({ name, about, avatar })
+  const { name, about, avatar, email, password } = req.body;
+  bcrypt.hash(req.body.password, 10).
+    then(hash =>
+  User.create({ name, about, avatar, email, password: hash }))
     .then((user) => res.send({ data: user }))
     .catch((err) => {
       if (err.name === 'ValidationError') {
--- models/user.js
+++ models/user.js
@@ -4,32 +4,29 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    role: {
-      name: String,
-      default: 'Жак-Ив Кусто',
-    },
+    default: 'Жак-Ив Кусто',
     minlength: 2,
     maxlength: 30,
   },
   about: {
     type: String,
-    role: {
-      about: String,
-      default: 'Исследователь',
-    },
+    default: 'Исследователь',
     minlength: 2,
     maxlength: 30,
   },
   avatar: {
     type: String,
-    role: {
-      avatar: String,
-      default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
-    },
+    default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
   },
   email: {
     type: String,
+    required: true,
+    unique: true,
   },
+  password: {
+    type: String,
+    required: true,
+  }
 });
 
 module.exports = mongoose.model('user', userSchema);
