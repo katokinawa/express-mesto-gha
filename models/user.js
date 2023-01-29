a7f037067c3edd718ca2da2616e6d03da2fbe8a4
Daniil Belogur <katokinawa@icloud.com>
2023-01-30 00:00:48 +0300

    feat: fix controllers login

--- models/user.js
+++ models/user.js
@@ -1,7 +1,6 @@
 // models/user.js
 const mongoose = require('mongoose');
 const isEmail = require('validator/lib/isEmail');
-const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -35,24 +34,5 @@ const userSchema = new mongoose.Schema({
     select: false,
   },
 });
-
-userSchema.statics.findUserByCredentials = function (email, password) {
-  return this.findOne({ email })
-    .select('+password')
-    .then((user) => {
-      if (!user) {
-        return Promise.reject(new Error('Неправильные почта или пароль'));
-      }
-
-      return bcrypt.compare(password, user.password)
-        .then((matched) => {
-          if (!matched) {
-            return Promise.reject(new Error('Неправильные почта или пароль'));
-          }
-
-          return user;
-        });
-    });
-};
 
 module.exports = mongoose.model('user', userSchema);
