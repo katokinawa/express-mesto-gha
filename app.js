563f9af40005f8628b16096eecccea4d8eb0a171
Daniil Belogur <katokinawa@icloud.com>
2023-02-15 00:23:28 +0300

    fix: update allowedCors correctly

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ app.use((req, res, next) => {
 
   if (allowedCors.includes(origin)) {
     // устанавливаем заголовок, который разрешает браузеру запросы с этого источника
-    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Origin', '*');
   }
 
   // Если это предварительный запрос, добавляем нужные заголовки
