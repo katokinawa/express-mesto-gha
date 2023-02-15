38a1872ab976495ebf89d971ad1d6a99d768efb0
Daniil Belogur <katokinawa@icloud.com>
2023-02-16 01:38:00 +0300

    feat: JWT

--- controllers/users.js
+++ controllers/users.js
@@ -122,7 +122,7 @@ module.exports.getCurrentUser = (req, res, next) => {
 
 module.exports.login = (req, res, next) => {
   const { email, password } = req.body;
-
+  console.log(1);
   return User.findUserByCredentials(email, password)
     .then((user) => {
       const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret', {
