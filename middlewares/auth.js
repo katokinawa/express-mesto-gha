66bc9de993bb579030a7afd3d14627283035b7b1
Daniil Belogur <katokinawa@icloud.com>
2023-02-16 16:55:05 +0300

    fix: bugs

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,11 +4,13 @@ const { NODE_ENV, JWT_SECRET } = process.env;
 const Unauthorized = require('../errors/Unauthorized');
 
 module.exports = (req, res, next) => {
-  const token = req.cookies.jwt;
+  const { authorization } = req.headers;
 
-  if (!token) {
+  if (!authorization || !authorization.startsWith('Bearer ')) {
     return next(new Unauthorized('Необходима авторизация'));
   }
+
+  const token = authorization.replace('Bearer ', '');
 
   let payload;
 
