2c3ab8596072c8cb1b8a102ff47547cd83dc9f18
Daniil Belogur <katokinawa@icloud.com>
2023-02-16 10:54:21 +0300

    fix: token

--- controllers/users.js
+++ controllers/users.js
@@ -133,7 +133,7 @@ module.exports.login = (req, res, next) => {
           maxAge: 3600000 * 24 * 7,
           httpOnly: true,
         })
-        .send({ message: 'Аутентификация успешна!' });
+        .send(token);
     })
     .catch((err) => {
       next(err);
